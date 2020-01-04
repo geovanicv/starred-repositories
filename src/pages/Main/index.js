03d8be51ec2f3d1335a46ef424a35f4bce4a80d8
Geovani Cavalcante <geovani.scavalcante@gmail.com>
2020-01-04 02:06:24 -0300

    Adicionando loading no botao ao pesquisar usuario

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {Keyboard} from 'react-native';
+import {Keyboard, ActivityIndicator} from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
 import api from '../../services/api';
@@ -22,10 +22,14 @@ export default class Main extends Component {
   state = {
     newUser: '',
     users: [],
+    loading: false,
   };
 
   handleAddUser = async () => {
     const {users, newUser} = this.state;
+
+    this.setState({loading: true});
+
     const response = await api.get(`/users/${newUser}`);
 
     const data = {
@@ -38,13 +42,14 @@ export default class Main extends Component {
     this.setState({
       users: [...users, data],
       newUser: '',
+      loading: false,
     });
 
     Keyboard.dismiss();
   };
 
   render() {
-    const {users, newUser} = this.state;
+    const {users, newUser, loading} = this.state;
 
     return (
       <Container>
@@ -53,12 +58,17 @@ export default class Main extends Component {
             autoCorrect={false}
             autoCapitalize="none"
             placeholder="Adicionar usuário"
+            value={newUser}
             onChangeText={text => this.setState({newUser: text})}
             returnKeyType="send"
             onSubmitEditing={this.handleAddUser}
           />
-          <SubmitButton onPress={this.handleAddUser}>
-            <Icon name="add" sixe={20} color="#FFF" />
+          <SubmitButton loading={loading} onPress={this.handleAddUser}>
+            {loading ? (
+              <ActivityIndicator color="#FFF" />
+            ) : (
+              <Icon name="add" sixe={20} color="#FFF" />
+            )}
           </SubmitButton>
         </Form>
 
