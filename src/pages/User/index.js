94af3c601e68f10f7b669dd8b4f76cb21dd35660
Geovani Cavalcante <geovani.scavalcante@gmail.com>
2020-01-07 16:39:59 -0300

    Imlementação do scroll infinito e pull to refresh

--- src/pages/User/index.js
+++ src/pages/User/index.js
@@ -24,20 +24,43 @@ export default class User extends Component {
 
   state = {
     stars: [],
+    page: 1,
     loading: true,
+    refreshing: false,
   };
 
-  async componentDidMount() {
+  async componentDidMount(page = 1) {
+    const {stars} = this.state;
     const {navigation} = this.props;
     const user = navigation.getParam('user');
 
-    const response = await api.get(`/users/${user.login}/starred`);
-    this.setState({stars: response.data, loading: false});
+    const response = await api.get(`/users/${user.login}/starred?page=${page}`);
+    this.setState({
+      stars: page >= 2 ? [...stars, ...response.data] : response.data,
+      page,
+      loading: false,
+      refreshing: false,
+    });
   }
+
+  loadMore = () => {
+    const {page} = this.state;
+    const nextPage = page + 1;
+    this.componentDidMount(nextPage);
+  };
+
+  refreshList = () => {
+    this.setState({
+      refreshing: true,
+      stars: [],
+    });
+
+    this.componentDidMount();
+  };
 
   render() {
     const {navigation} = this.props;
-    const {stars, loading} = this.state;
+    const {stars, loading, refreshing} = this.state;
 
     const user = navigation.getParam('user');
 
@@ -52,6 +75,10 @@ export default class User extends Component {
           <Loading size="small" color="#7159c1" />
         ) : (
           <Stars
+            onRefresh={this.refreshList}
+            refreshing={refreshing}
+            onEndReachedThreshold={0.2}
+            onEndReached={this.loadMore}
             data={stars}
             keyExtractor={star => String(star.id)}
             renderItem={({item}) => (
